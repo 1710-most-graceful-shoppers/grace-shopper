a496ad7dff2c2f9cab520ab3087152a78a107ec1
Mark Lopez <molopez492@gmail.com>
2018-01-09 17:43:44 -0500

    Product routes

---
+++ server/api/products.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+// const {Product} = require('../db/models')
+module.exports = router
+
+router.param('id', (req, res, next, id) => {
+  // Product.findById(id)
+  //   .then(product => {
+  //     if (!product) return console.log("No product found")
+  //     req.product = product
+  //     next()
+  //   })
+  //   .catch(next)
+})
+
+router.get('/', (req, res, next) => {
+  // Product.findAll()
+  //   .then(products => res.send(products))
+  //   .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  // Product.create(req.body)
+  //   .then(product => res.send(product))
+  //   .catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  req.product.update(req.body)
+    .then(product => res.send(product))
+    .catch(next)
+})
