068124d0efad9ef37a9e38ec0ab04373783a7eb0
BFriedman117 <binyaminf@yahoo.com>
2018-01-10 17:44:10 -0500

    product filter working

--- client/components/products.js
+++ client/components/products.js
@@ -1,42 +1,70 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 
-const Products = (props) => {
+class Products extends Component {
+  constructor(props){
+    super(props);
 
-  const {products} = props
+    this.state = {
+      input: ''
+    }
 
-  return (
-    <div>
-      <h1>All Products</h1>
-      <div className="product-container">
-        {
-          products.map(product => (
-            <NavLink to={`/products/${product.id}`} key={product.id}>
-            <div className="card">
-              <div>
-                <div>
-                  <img src={product.imageUrl} className="product-image"/>
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  render(){
+
+    const products = this.props.products.filter(product => product.title.toLowerCase().match(this.state.input))
+
+    return (
+      <div>
+        <div className="product-header">
+          <h1 className="product-title">All Products</h1>
+          <form className="product-filter" style={{marginTop: '20px'}}>
+            <input
+              className="product-filter-input"
+              placeholder="Filter Products"
+              onChange={this.handleChange}
+              />
+          </form>
+        </div>
+        <div className="product-container">
+          {
+            products.map(product => (
+              <NavLink to={`/products/${product.id}`} key={product.id}>
+                <div className="card">
+                  <div>
+                    <div>
+                      <img src={product.imageUrl} className="product-image" />
+                    </div>
+                  </div>
+                  <div className="product-info">
+                    <div className="product-name">
+                      {product.title}
+                    </div>
+                    <div className="product-price">
+                      {product.price} Coins
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="product-info">
-                <div className="product-name">
-                  {product.title}
-                </div>
-                <div className="product-price">
-                  {product.price} Coins
-                </div>
-              </div>
-            </div>
-          </NavLink>
+              </NavLink>
             )
           )
         }
       </div>
     </div>
   )
+  }
+
+handleChange(event){
+  this.setState({
+    input: event.target.value.toLowerCase()
+  })
+}
+
 }
 
 /**
@@ -48,9 +76,7 @@ const mapState = (state) => {
   }
 }
 
-const mapDispatch = (dispatch) => {
-  return {}
-}
+const mapDispatch = null;
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
