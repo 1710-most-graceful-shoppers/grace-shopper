c5b985abb75eb40b52fafe849e5169fc6cc5cc85
Sarah Zhao <sarahzhao25@gmail.com>
2018-01-10 17:48:17 -0500

    started categories

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,8 +1,30 @@
 const CategoryRouter = require('express').Router();
 const {Category, Product} = require('../db/models');
 
+CategoryRouter.param('id', (req, res, next, id) => {
+  Category.findById(Number(id), {include: [{model: Product}]})
+  .then(category => {
+    if (!category) {
+      let err = new Error('No category found')
+      err.status = 404;
+      next(err);
+    }
+    else {
+      req.category = category;
+      next();
+    }
+  })
+  .catch(next);
+})
+
 CategoryRouter.get('/', (req, res, next) => {
   Category.findAll()
   .then(categories => res.json(categories))
   .catch(next);
 })
+
+CategoryRouter.get('/:id', (req, res, next) => {
+  res.json(req.category)
+})
+
+module.exports = CategoryRouter;
