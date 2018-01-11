3f9dc923f4af1fae14c5c265eb26beee3fcef4a5
Sarah Zhao <sarahzhao25@gmail.com>
2018-01-11 11:15:00 -0500

    Changed products.js to deal with merge conflicts between search function and cart information

--- client/components/products.js
+++ client/components/products.js
@@ -1,47 +1,72 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import {addCartIdToSession} from '../store';
 
 
-const Products = (props) => {
+class Products extends Component {
 
-  const {products, addToCart} = props
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    <div>
-      <h1>All Products</h1>
-      <div className="product-container">
-        {
-          products.map(product => (
-            <div key={product.id}>
-            <NavLink to={`/products/${product.id}`} >
-            <div className="card">
-              <div>
-                <div>
-                  <img src={product.imageUrl} className="product-image"/>
-                </div>
-              </div>
-              <div className="product-info">
-                <div className="product-name">
-                  {product.title}
-                </div>
-                <div className="product-price">
-                  {product.price} Coins
-                </div>
-              </div>
-            </div>
-          </NavLink>
-          <button onClick={() => addToCart(product.id)} >Add me to cart!
-          </button>
+  render() {
+    const {addToCart} = this.props
+    const products = this.props.products.filter(product => product.title.toLowerCase().match(this.state.input))
+
+    return (
+      <div>
+        <div className="product-header">
+          <h1 className="product-title">All Products</h1>
+            <form className="product-filter" style={{marginTop: '20px'}}>
+              <input
+              className="product-filter-input"
+              placeholder="Filter Products"
+              onChange={this.handleChange}
+              />
+            </form>
           </div>
+        <div className="product-container">
+          {
+            products.map(product => (
+              <div key={product.id}>
+              <NavLink to={`/products/${product.id}`} >
+              <div className="card">
+                <div>
+                  <div>
+                    <img src={product.imageUrl} className="product-image"/>
+                  </div>
+                </div>
+                <div className="product-info">
+                  <div className="product-name">
+                    {product.title}
+                  </div>
+                  <div className="product-price">
+                    {product.price} Coins
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+            <button onClick={() => addToCart(product.id)} >Add me to cart!
+            </button>
+            </div>
+              )
             )
-          )
-        }
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value.toLowerCase()
+    })
+  }
+
 }
 
 /**
