9c65c74c20fbd7fa77b6d986e130b1d96d79ceb7
Sarah Zhao <sarahzhao25@gmail.com>
2018-01-10 20:41:56 -0500

    Updated seed file, and category backend

--- client/components/main.js
+++ client/components/main.js
@@ -57,7 +57,7 @@ const mapDispatch = (dispatch) => {
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Main))
+export default connect(mapState, mapDispatch)(Main)
 
 /**
  * PROP TYPES
