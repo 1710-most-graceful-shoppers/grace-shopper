b6e390413614212fda4b37e7ca34dc58c6a42331
Mark Lopez <molopez492@gmail.com>
2018-01-16 18:36:00 -0500

    totalcost fix

--- client/components/Orders.js
+++ client/components/Orders.js
@@ -15,42 +15,44 @@ class Orders extends Component {
 
   render() {
     const { orders, isAdmin } = this.props;
-    let totalCost = 0;
     return (
       <div>
         <h1>Orders</h1>
         <div className="orders-container">
           {
-            orders.map(order => (
-              <div key={order.id}>
-                <div className="order">
-                  <div className="order-info">
-                    <div className="order-id">
-                      <h3>Order ID {order.id}</h3>
+            orders.map(order => {
+              let totalCost = 0;
+              return (
+                <div key={order.id}>
+                  <div className="order">
+                    <div className="order-info">
+                      <div className="order-id">
+                        <h3>Order ID {order.id}</h3>
+                      </div>
+                      <div className="order-date">
+                        <h3>Order Date {order.createdAt.slice(0, 10)}</h3>
+                      </div>
                     </div>
-                    <div className="order-date">
-                      <h3>Order Date {order.createdAt.slice(0, 10)}</h3>
+                    <div className="product-info">
+                      {
+                        order.products.map(product => {
+                          console.log(totalCost)
+                          totalCost += product.product_order.quantity * product.product_order.price;
+                          return (
+                            <div key={product.id} className="cart-product">
+                              <Link to={`/products/weapons/${product.id}`}><h4>Name: {product.title}</h4></Link>
+                              <h4>Quantity: {product.product_order.quantity} ; Price: {product.product_order.price} coins ; Subtotal: {product.product_order.price * product.product_order.quantity} coins</h4>
+                            </div>
+                          )
+                        })}
                     </div>
-                  </div>
-                  <div className="product-info">
-                    {
-                      order.products.map(product => {
-                        console.log(totalCost)
-                        totalCost += product.product_order.quantity * product.product_order.price;
-                        return (
-                          <div key={product.id} className="cart-product">
-                            <Link to={`/products/weapons/${product.id}`}><h4>Name: {product.title}</h4></Link>
-                            <h4>Quantity: {product.product_order.quantity} ; Price: {product.product_order.price} coins ; Subtotal: {product.product_order.price * product.product_order.quantity} coins</h4>
-                          </div>
-                        )
-                    })}
-                  </div>
-                  <div className="order-total">
-                    <h3>Total Cost: {order.coreyPromo ? totalCost / 2 : totalCost} coins {order.coreyPromo ? '(Promo Applied)' : '(No Promo Code Found)'}</h3>
+                    <div className="order-total">
+                      <h3>Total Cost: {order.coreyPromo ? totalCost / 2 : totalCost} coins {order.coreyPromo ? '(Promo Applied)' : '(No Promo Code Found)'}</h3>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
+              )
+            })
           }
         </div>
       </div>
